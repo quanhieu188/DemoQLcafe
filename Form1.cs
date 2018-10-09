8fbb65bf9507c726a92eeaf98a5204590d3a26a9
B23 <quanhieu18898@gmail.com>
2018-10-09 15:10:17 +0700

    button 1

--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,7 @@ namespace DemoQLcafe
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (txtA.Text != String.Empty && txtB.Text != String.Empty)
+            if (txtA.Text != String.Empty)
             {
                 double a = double.Parse(txtA.Text);
                 double b = double.Parse(txtB.Text);
